c9b1f042f6cc858972fbf08b11f2e90bcdc148fa
Peter Choi <cbj0618@gmail.com>
2017-07-24 19:28:46 +0900

    쿼리스트링 파싱 예제

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const app = express();
 // app.use(express.static('public'));
 app.locals.pretty = true; // 페이지 소스 보기 할 때 이쁘게 보기!!
 app.set('view engine', 'pug');
-// app.set('views', './views');
+app.set('views', './views'); // 생략 가능
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.send('Welcome!!!');
+});
+
+app.get('/topic', (req, res) => {
+  let id = req.query.id;
+  var topics = [
+    'Javascript is ...',
+    'Nodejs is ...',
+    'Express is ...'
+  ];
+  var output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br>
+  ${topics[id]}
+  `
+  res.send(output);
 });
 
 app.get('/template', (req, res) => {
