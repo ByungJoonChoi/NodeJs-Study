9ebfd0ae6537f300c6869979bd20172b0a1cbd8d
Peter Choi <cbj0618@gmail.com>
2017-07-24 18:41:23 +0900

    템플릿 엔진 'pug' 추가 템플릿 엔진 렌더링 예제 라우터('/template') 추가

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 // app.use(express.static('public'));
+app.locals.pretty = true; // 페이지 소스 보기 할 때 이쁘게 보기!!
+app.set('view engine', 'pug');
+// app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.send('Welcome!!!');
+});
+
+app.get('/template', (req, res) => {
+  res.render('temp', {time: new Date().toLocaleString(), _title: "pug"});
 });
 
 app.get('/login', (req, res) =>{
