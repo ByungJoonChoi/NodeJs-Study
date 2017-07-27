a2e761b9b1a1ef4590fd653f0837d56f404366d8
Peter Choi <cbj0618@gmail.com>
2017-07-27 20:52:47 +0900

    파일업로드 - 2 multer를 이용한 서버 local disk 저장 로직 구현

--- app_file.js
+++ app_file.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+const upload = multer({ storage: storage });
 const app = express();
 const fs = require('fs');
 app.locals.pretty = true; // 페이지 소스 보기 할 때 이쁘게 보기!!
@@ -12,9 +21,10 @@ app.get('/upload', (req, res) => {
   res.render('uploadform');
 });
 
-app.post('/upload', (req, res) => {
-  res.send("upload post");
+app.post('/upload', upload.single('userfile'), (req, res) => {
+  res.send(req.file.filename);
 });
+
 app.get('/topic/new', (req,res) =>{
   fs.readdir('data', (err,files) => {
     if(err){
