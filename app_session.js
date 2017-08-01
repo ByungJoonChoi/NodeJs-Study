0f68c52bcae7cc203f8867e73bc19e48c7044500
Peter Choi <cbj0618@gmail.com>
2017-08-01 22:37:18 +0900

    express-session 모듈 응용 - 로그인 기능 구현 - session에 displayName 정보 저장 여부에 따라 로그인 상태를 판단함. - 로그인은 session.displayName 에 user의 displayName을 저장 - 로그아웃은 session.displayName property 제거

--- app_session.js
+++ app_session.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session')
+const bodyParser = require('body-parser')
 const app = express();
 app.use(session({
   secret: 'kdasjf093j9qf03jf',
@@ -8,6 +9,60 @@ app.use(session({
 }))
 // secret : session을 쿠키에 저장할 때 암호화할 때 사용하는 키?
 // session()은 기본적으로 정보를 메모리에 저장
+app.use(bodyParser.urlencoded({extended: false}));
+app.get('/auth/login', (req, res) => {
+  var output = `
+  <h1>Login</h1>
+  <form action="/auth/login" method="post">
+    <p>
+      <input type="text" name="username" placeholder="username">
+    </p>
+    <p>
+      <input type="password" name="password" placeholder="password">
+    </p>
+    <p>
+      <input type="submit">
+    </p>
+  </form>
+  `;
+  res.send(output);
+});
+
+app.post('/auth/login', (req, res) => {
+  let user = {
+    username:'peter',
+    password:'9999',
+    displayName:'BJ Choi'
+  }
+  let uname = req.body.username;
+  let pwd = req.body.password;
+  if(user.username === uname && user.password === pwd){
+    req.session.displayName = user.displayName;
+    res.redirect('/welcome');
+  } else {
+    delete req.session.displayName;
+    res.send('Who are you? <a href="/auth/login">login</a>');
+  }
+});
+
+app.get('/welcome', (req,res) => {
+  if(req.session.displayName){
+    res.send(`
+      <h1>Hello, ${req.session.displayName}</h1>
+      <a href="/auth/logout">LogOut</a>
+    `);
+  } else {
+    res.send(`
+      <h1>Welcome</h1>
+      <a href="/auth/login">Login</a>
+    `);
+  }
+})
+
+app.get('/auth/logout', (req, res) => {
+  delete req.session.displayName;
+  res.redirect('/welcome');
+});
 
 app.get('/count', (req, res) => {
   if(req.session.count){
