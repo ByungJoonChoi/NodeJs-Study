f1ccf0b2ab2429d33639385d94dec52ec7fa3a9d
Peter Choi <cbj0618@gmail.com>
2017-08-01 20:43:47 +0900

    cookie-parser 응용 예제 - 장바구니

--- app_cookie.js
+++ app_cookie.js
@@ -3,6 +3,57 @@ const cookieParser = require('cookie-parser')
 const app = express();
 app.use(cookieParser());
 
+var products = {
+  1:{title:"스타크래프트 리마스터"},
+  2:{title:"오버워치"}
+};
+
+app.get('/products', (req,res) => {
+  var output = '';
+  for (var name in products){
+    output += `<li>${products[name].title}<a href="/cart/${name}">add</a></li>`
+  }
+  res.send(`<h1>Products List</h1>
+    <ul>${output}</ul>
+    <a href="/cart">Cart</a>`);
+})
+/*
+웹브라우져 쿠키에 cart
+cart {
+  1:3  //{product id}:{수량}
+  2:5
+}
+*/
+app.get('/cart/:id', (req,res) => {
+  let id = req.params.id;
+  if(req.cookies.cart){
+    var cart = req.cookies.cart;
+  } else{
+    cart = {};
+  }
+  if(!cart[id]){
+    cart[id] = 0;
+  }
+  cart[id] = parseInt(cart[id]) + 1;
+  res.cookie('cart', cart);
+  res.redirect('/cart');
+});
+
+app.get('/cart', (req,res)=>{
+  let cart = req.cookies.cart;
+  if(!cart){
+    res.send("Empty cart!! You hava to buy something!!");
+    return;
+  }
+  var output = '';
+  for(var id in cart){
+    let title = products[id].title;
+    let count = cart[id];
+    output += `<li>${title} (${count})</li>`
+  }
+  res.send(`<h1>Cart</h1><ul>${output}</ul><a href="/products">Products List</a>`);
+});
+
 app.get('/count', (req,res) => {
   console.log('Cookies: ', req.cookies);
   let count = parseInt(req.cookies.count);
