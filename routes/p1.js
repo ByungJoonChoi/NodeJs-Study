cca1ebcffbb5b9912d4d07c0c41a932d422a4d42
Peter Choi <cbj0618@gmail.com>
2017-08-12 15:27:35 +0900

    라우트 분리하기 (모듈에 Entry 변수 전달) - require('./routes/p1')(app)과 같이 선언하면 모듈에 app 변수 전달이 가능하다. - p1.js 모듈에서 module.exports = function(app){ //app 사용} 와 같이 변수를 받아서 사용할 수 있다.

--- routes/p1.js
+++ routes/p1.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var route = express.Router();
-route.get('/r1', (req, res) => {
-  res.send("Hello /p1/r1");
-})
-route.get('/r2', (req, res) => {
-  res.send("Hello /p1/r2");
-})
-module.exports = route;
+module.exports = function(app){
+  var express = require('express');
+  var route = express.Router();
+  route.get('/r1', (req, res) => {
+    res.send("Hello /p1/r1");
+  })
+  route.get('/r2', (req, res) => {
+    res.send("Hello /p1/r2");
+  })
+  app.get('/p3', (req, res) => {
+    res.send("p3!!")
+  })
+  return route;
+}
