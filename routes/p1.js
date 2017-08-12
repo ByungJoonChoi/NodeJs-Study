71a077ff2e27e27ac4435ac66cca0cb12a075e17
Peter Choi <cbj0618@gmail.com>
2017-08-12 13:15:58 +0900

    라우트 분리하기 (파일 분리) - 모듈과 express.Router를 이용하여 라우트를 분리할 수 있다. - app.use('/p1', router) : /p1 이하로 들어오는 요청에 대한 처리를 router에 위임한다.

---
+++ routes/p1.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var route = express.Router();
+route.get('/r1', (req, res) => {
+  res.send("Hello /p1/r1");
+})
+route.get('/r2', (req, res) => {
+  res.send("Hello /p1/r2");
+})
+module.exports = route;
