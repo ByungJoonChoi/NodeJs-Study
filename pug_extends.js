7c52e1721e237ab0125ed26b4701b0e74391ead6
Peter Choi <cbj0618@gmail.com>
2017-08-12 11:58:48 +0900

    pug extends 연습을 위한 예제 코드 작성

---
+++ pug_extends.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'pug');
+app.get('/view', (req, res) => {
+  res.render('view')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+app.listen(3003, () => {
+  console.log('Connect 3003 port')
+})
