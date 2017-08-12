d4985d511ef7ef3eaada1313f60a0b1f052502d0
Peter Choi <cbj0618@gmail.com>
2017-08-12 12:13:26 +0900

    pug extends 적용 - add.pug, view.pug의 중복코드를 layout.pug로 옮기고, view specific 한 부분은 "block content"라는 이름으로 정의함 - add.pug, view.pug에서는 "extends ./layout" 키워드를 이용하여 코드를 로드하며, view specific한 부분은 "block content"를 입력한 뒤 그 아래 들여쓰기를 해서 내용을 채워넣음.

--- pug_extends.js
+++ pug_extends.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.locals.pretty = true; // 페이지 소스 보기 할 때 이쁘게 보기!!
 app.set('view engine', 'pug');
 app.set('views', 'pug');
 app.get('/view', (req, res) => {
